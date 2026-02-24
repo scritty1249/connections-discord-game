28e4c71475bc00f0d2344428cf5593b2f8dfbcdd
Kyle <kaisonpham@gmail.com>
2026-02-24 14:58:39 -0800

    Figuring out vercel project structure

--- app/api/gamedata.js
+++ app/api/gamedata.js
@@ -1,6 +1,13 @@
-import { fetchGameData } from "/lib/server.js" 
+import { fetchGameData } from "/lib/server.js"
 
 export default async function handler(req, res) {
-    const data = await fetchGameData(new Date());
-    res.status(200).json(data);
+    try {
+        const data = await fetchGameData(new Date())
+        res.status(200).json(data)
+    } catch (error) {
+        console.error("Fetch error:", error)
+        response
+            .status(500)
+            .json({ message: "Error fetching data from external resource" })
+    }
 }
