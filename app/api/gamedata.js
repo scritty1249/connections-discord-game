a6d7d0fb8275f6d638a5fb75f03bb8a956e6ee82
Kyle <kaisonpham@gmail.com>
2026-02-24 17:43:38 -0800

    Added gamedata api endpoint

--- app/api/gamedata.js
+++
@@ -1,1 +0,0 @@
-// gets stored gamedata
--- app/api/get-gamedata.js
+++ app/api/get-gamedata.js
@@ -1,9 +1,9 @@
 
-import { fetchGameData } from "../lib/server.js"
+import { getGameData } from "../lib/server.js"
 
 export default async function handler(req, res) {
     try {
-        const data = await fetchGameData(new Date());
+        const data = await getGameData();
         res.status(200).json(data);
     } catch (err) {
         console.error("Fetch error:", err);
