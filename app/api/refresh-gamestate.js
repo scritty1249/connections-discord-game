70009c629a4da8de5c532138116a9ab4dab282b5
Kyle <kaisonpham@gmail.com>
2026-02-24 17:18:27 -0800

    Testing refresh cron job

--- app/api/refresh-gamestate.js
+++ app/api/refresh-gamestate.js
@@ -3,15 +3,15 @@ import { fetchGameData, storeGameData, wipeAttempts } from "../lib/server.js"
 export default async function handler(req, res) {
     try {
         const data = await fetchGameData(new Date());
-        await storeGameData(data);
+        console.log(await storeGameData(data));
         console.log("Saved gamedata successfully");
         await wipeAttempts();
         console.log("Cleared userdata successfully");
-        res.status(200);
+        res.status(200).json({status: "OK"});
     } catch (err) {
         console.error("Execution error:", err);
         res
             .status(500)
-            .json({ message: err.message });
+            .json({ error: err.message });
     }
 }
