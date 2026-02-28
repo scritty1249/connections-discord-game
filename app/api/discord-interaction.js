ad500ae60e005c43e19f28cd5c60121fe927a3eb
Kyle <kaisonpham@gmail.com>
2026-02-27 18:47:04 -0800

    im dumb

--- app/api/discord-interaction.js
+++ app/api/discord-interaction.js
@@ -1,20 +1,25 @@
 import { verifyDiscordRequest } from "../lib/server.js";
 
 export async function POST(req) {
-    const { type, data } = await req.json();
-    if (!verifyDiscordRequest(req)) return new Response(null, {status: 401}); // specified by discord api guidelines
-    switch (type) {
-        case 2: // APPLICATION COMMAND
-        // [!] holy aids nested switch, clean this up later...
-            switch (data?.type) { // shouldn't be null
-                case 4: // PRIMARY ENTRY POINT
-                    switch (data?.name?.toLowerCase()) {
-                        case "launch":
-                            return Response.json({type: 12});
-                    };
-            };
-        case 1: // PING
-        default:
-            return Response.json({type: 1}); // ACK/PONG
-    };
+    try {
+        const { type, data } = await req.json();
+        if (!verifyDiscordRequest(req)) return new Response(null, {status: 401}); // specified by discord api guidelines
+        switch (type) {
+            case 2: // APPLICATION COMMAND
+            // [!] holy aids nested switch, clean this up later...
+                switch (data?.type) { // shouldn't be null
+                    case 4: // PRIMARY ENTRY POINT
+                        switch (data?.name?.toLowerCase()) {
+                            case "launch":
+                                return Response.json({type: 12});
+                        };
+                };
+            case 1: // PING
+            default:
+                return Response.json({type: 1}); // ACK/PONG
+        };
+    } catch (err) {
+        console.error("Execution error:", err);
+        return Response.json({error: err.message}, {status: 500, statusText: "Internal server error"});
+    }
 }
