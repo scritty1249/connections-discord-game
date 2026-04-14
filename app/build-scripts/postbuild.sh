c8cf3d11437248ac05f659d80319bff48343e696
Kyle <kaisonpham@gmail.com>
2026-04-14 16:06:57 -0700

    Discontinued postbuild script

--- app/build-scripts/postbuild.sh
+++ app/build-scripts/postbuild.sh
@@ -1,5 +1,7 @@
+# doesn't work for activities
+
 current_dt=$(TZ="America/Los_Angeles" date +"%-I:%M%p (PST), %d/%m/%Y")
-curl -g -X PATCH https://discord.com/api/v10/users/@me \
-    -H "Authorization: Bot "$DISCORD_BOT_TOKEN \
+curl -X PATCH "https://discord.com/api/v10/users/@me" \
+    -H "Authorization: Bot $DISCORD_BOT_TOKEN" \
     -H "Content-Type: application/json" \
-    -d '{"bio": "Last Updated at '$current_dt'"}'
+    -d "{\"bio\": \"The unoffical NYT Connections game\n\nLast updated: $current_dt\"}"
