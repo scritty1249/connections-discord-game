90b5e8b0a338765eedd084cce2a5618ec5888197
Kyle <84257126+scritty1249@users.noreply.github.com>
2026-04-20 05:22:27 -0700

    Fixed animation bug

--- app/client/script/main.js
+++ app/client/script/main.js
@@ -178,7 +178,8 @@ function submitHandler (e) {
 
 function playCorrectAttemptAnimation (categoryEl, wordEls, wordContainer, categoryContainer) {
     const sortedWordEls = wordEls.filter(wordEl => parseInt(wordEl.style.order) >= 4).toSorted((a, b) => parseInt(a.dataset.id) - parseInt(b.dataset.id));
-    const topRowWordEls = ELEMENTS.WORDS.filter(wordEl => parseInt(wordEl.style.order) < 4 && !wordEls.includes(wordEl)).sort((a, b) => parseInt(a.style.order) - parseInt(b.style.order)); // [!] horrible
+    const startIdx = ELEMENTS.CATEGORY_GRID.children.length * 4;
+    const topRowWordEls = ELEMENTS.WORDS.filter(wordEl => parseInt(wordEl.style.order) >= startIdx && parseInt(wordEl.style.order) < startIdx + 4 && !wordEls.includes(wordEl)).sort((a, b) => parseInt(a.style.order) - parseInt(b.style.order)); // [!] horrible
     return Promise.all(Array.from(sortedWordEls, (wordEl, idx) =>
         cardFX.swapElements(wordEl, topRowWordEls[idx])))
     .then(() => {
