fd03a249197f6b6b586415437935527de2d1f7e0
Kyle <kaisonpham@gmail.com>
2026-04-19 17:36:15 -0700

    Removed redundant functions

--- app/client/script/utils.js
+++ app/client/script/utils.js
@@ -1,7 +1,3 @@
-export function isOverflowed (el) {
-  return el.scrollHeight > el.clientHeight || el.scrollWidth > el.clientWidth;
-}
-
 export function attemptIsCorrect (attempt, categories) { // categories is an Array of category word ids ([[Number, ...], ...])
     // expects attempt as Array of Numbers, nested categories should already be sorted
     return categories.some(category =>
