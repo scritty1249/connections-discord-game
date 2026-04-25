019df143b0333980a3bcf43e2af9dbaa24a5ddcb
Kyle <kaisonpham@gmail.com>
2026-04-24 17:10:30 -0700

    Fixed typo in prebuild phase

--- app/build-scripts/prebuild.sh
+++ app/build-scripts/prebuild.sh
@@ -1,2 +1,2 @@
 sed -i "s|<!--BUILD_ID-->|"$VERCEL_GIT_COMMIT_SHA"|g" client/index.html
-sed -i "s|<!--BUILD_ID_SHORT-->|"${$VERCEL_GIT_COMMIT_SHA:0:7}"|g" client/index.html
+sed -i "s|<!--BUILD_ID_SHORT-->|"${VERCEL_GIT_COMMIT_SHA:0:7}"|g" client/index.html
