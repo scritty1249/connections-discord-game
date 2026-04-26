114df03ff1e5b24bc68d040a6f01da959842a8b6
Kyle <kaisonpham@gmail.com>
2026-04-26 15:43:28 -0700

    Added debug message

--- app/api/discord-interaction.js
+++ app/api/discord-interaction.js
@@ -30,6 +30,7 @@ export async function POST(req) {
                             case 1: // DM with the bot only
                                 switch (commandName) {
                                     case "api":
+                                        console.debug(requestBody);
                                         // api subcommand interactions should include an option field
                                         const { options } = data;
                                         const subCommandName = options?.[0]?.name?.toLowerCase();
