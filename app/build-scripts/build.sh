87fc05f7b5a93bc33aa2e7eb96623a7bb1f46588
Kyle <kaisonpham@gmail.com>
2026-04-27 16:33:39 -0700

    Attempting to fix bug that causes buttons to be invisible on mobile devices
    RESOLVING CACHING ISSUE

--- app/build-scripts/build.sh
+++ app/build-scripts/build.sh
@@ -1,4 +1,7 @@
 mkdir -p public/stylesheets
 cp client/index.html public/index.html
-cp -a client/stylesheets/. public/stylesheets/
+for file in client/stylesheets/*.css; do
+    filename=$(basename "$file" .css)
+    cp "$file" "public/stylesheets/${filename}-$VERCEL_GIT_COMMIT_SHA.css"
+done
 esbuild client/script/main.js --bundle --minify --outfile="public/script/bundle-"$VERCEL_GIT_COMMIT_SHA".js"
